3cdd55c0d6e7fe21d6dc2d688270cfb84b7c43ea
Bruno <brunoppass@gmail.com>
2020-11-03 11:29:33 -0300

    master

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from '../base/Button';
+import {action} from '@storybook/addon-actions'
+import '../styles/index.css'
+
+export default {
+  title: 'Button',
+  component: Button
+};
+
+const Template = args => <div style={{width: '200px'}}>{<Button {...args} />}</div>;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  isPrincipal: true,
+  value: 'Button',
+  handleClick: ()=>{}
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  isPrincipal: false,
+  value: 'Button',
+  handleClick: ()=>{}
+};
---
+++ src/stories/Card.stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from '../base/Card'
+import '../styles/index.css'
+
+export default {
+  title: 'Card',
+  component: Card
+};
+
+const Template = children => 
+  <div style={{width:'400px'}}> 
+    <Card>
+      <h3 className="text__h3--black">This is a Card</h3>
+      <p className="text__p">Welcome!</p>
+    </Card> 
+  </div>
+
+export const Primary = Template.bind({})
