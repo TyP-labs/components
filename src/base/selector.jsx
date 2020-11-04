9aaf4845101e17e122930e93d3909276aa851ec8
Bruno <brunoppass@gmail.com>
2020-11-03 21:43:15 -0300

    master

--- src/base/selector.jsx
+++ src/base/selector.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 
+/**
+ * Selector UI Base Component
+ * @param {string} value string value
+ * @param {*} handleClick function to handle click event
+ * @return Selector
+ */
 const Selector=(props)=>{ 
 
-    const{value,handleClick}=props
+    const { value, handleClick } = props
+
     const propieties={
         value:value, 
         type:"button",
-        className:"selector"
+        className:"selector",
+        onClick: handleClick
     }
 
     return(
         <input {...propieties}/>
     )
 }
- 
-
-
-
-
-
 
 export default Selector
