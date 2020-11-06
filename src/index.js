a73fd29ba52f8fcc2f02d035a16a7c68ad9b3cbb
Bruno <brunoppass@gmail.com>
2020-11-06 15:55:40 -0300

    master

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import reportWebVitals from './reportWebVitals';
-import Router from './Router';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
-
-reportWebVitals();
