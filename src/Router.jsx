9438e47b92810758e8509bfa82a6720e090231c2
Bruno <brunoppass@gmail.com>
2020-11-19 11:18:53 -0300

    master

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,14 +5,17 @@ import Landing from './pages/Landing/Landing';
 import SignUp from './pages/SignUp';
 import SignUp__Profesor from './pages/SignUp__Profesor';
 
-
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/crear-cuenta" component={SignUp} />
-        <Route exact path="/crear-cuenta/profesor" component={SignUp__Profesor} />
+        <Route
+          exact
+          path="/crear-cuenta/profesor"
+          component={SignUp__Profesor}
+        />
       </Switch>
       <Footer
         list={[
